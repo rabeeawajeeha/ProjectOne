008a718fc7197013c1764efe972e4517740de88a
Aaron Colbert <aaron-colbert@hotmail.com>
2018-06-02 18:07:22 -0400

    Completed walmart ajax. Added functions for Dryer code, added Comments to all fucntions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,6 @@ $("#add-user").on("click", function (event)
 
     name = $("#name-input").val().trim();
 
-
     // Code for the push
     dataRef.ref().push({
         name: name,
@@ -32,10 +31,8 @@ $("#add-user").on("click", function (event)
 // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
 dataRef.ref().on("child_added", function (childSnapshot)
 {
-
     // Log everything that's coming out of snapshot
     console.log(childSnapshot.val().name);
-
 
     // full list of items to the well
     $("#full-member-list").append("<div class='well'><span class='member-name'> " + childSnapshot.val().name);
@@ -56,7 +53,17 @@ dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functio
 
 ///////////////////////////////////////////AARON WORKING SECTION////////////////////////////////////////////////////
 
-var SendWalmartParams = function (searchTerm, numItems, categoryId)
+InitializePage();
+/*
+Description: This function Creates a JSON Object of parameter to send to the GetWalmartProduct function to perform an ajax call
+Parameters: searchTerm - string for ajax call to search Keyword
+            numItems - string to limit the number of results return from ajax call
+            categoryId - string an Id provided to seach with in a specific category in the walmart api
+Created By: Aaron Colbert
+Created: 05/28/2018
+LastModified: 06/02/2018
+ */
+var SendWalmartParams = function (searchTerm, numItems, categoryId,jqueryRef)
 {
     console.log(searchTerm);
     console.log(numItems);
@@ -96,10 +103,18 @@ var SendWalmartParams = function (searchTerm, numItems, categoryId)
     }
 
     console.log("PARAMSEND" + JSON.stringify(base))
-    GetWalmartProduct(base)
+    GetWalmartProduct(base,jqueryRef)
 }
 
-var GetWalmartProduct = function (queryParams)
+/*
+Description: This function contains an ajax call to the walmart api to retrive specific information of a product and calls the
+            DrawProductCard function to display the information
+Parameters: queryParams - Object of query parameter for the ajax call basic parameter are found in the SendWalmartParams function
+Created By: Aaron Colbert
+Created: 05/26/2018
+LastModified: 06/02/2018
+ */
+var GetWalmartProduct = function (queryParams,jqueryRef)
 {
     console.log("GETWLAMARTPROD" + JSON.stringify(queryParams))
     var url = "http://api.walmartlabs.com/v1/search";
@@ -113,25 +128,32 @@ var GetWalmartProduct = function (queryParams)
     }).done(function (result)
     {
         var Item = result.items
-        for (var i = 0; i < Item.length; i++)
+        if (Item.length == 0)
         {
-            console.log(result);
-            console.log(Item[i].name);
-            console.log(Item[i].salePrice);
-            console.log(Item[i].upc);
-            console.log(Item[i].largeImage);
-            console.log(Item[i].productUrl);
-
-            DrawProductCard(Item[i].name, Item[i].salePrice, Item[i].upc, Item[i].largeImage, Item[i].productUrl)
+            //Display a No Results Message
         }
+        else
+        {
+            for (var i = 0; i < Item.length; i++)
+            {
+                console.log(result);
+                console.log(Item[i].name);
+                console.log(Item[i].salePrice);
+                console.log(Item[i].upc);
+                console.log(Item[i].largeImage);
+                console.log(Item[i].productUrl);
 
-
+                DrawProductCard(Item[i].name, Item[i].salePrice, Item[i].upc, Item[i].largeImage, Item[i].productUrl, jqueryRef)
+            }
+        }
     }).fail(function (err)
     {
         throw err;
     });
 }
-SendWalmartParams("Ipod", 3);
+
+
+
 
 //If we want to do category search we need to defione the categories
 //And then I can use the API to designate category id with in 
@@ -164,8 +186,18 @@ categoryNode
 categoryPath
 */
 
-
-var DrawProductCard = function (name, price, upc, imageSrc, siteLink)
+/*
+Description: This function Creates the column that displayes a single product and it's information that is added to a row
+Parameters: name - stringvalue for the product Title
+            price - string value for the Product Price
+            upc - string value to be stored in an attribute for Product details
+            imageSrc - Source image of product
+            siteLink - Link to the actual site of the product
+Created By: Aaron Colbert
+Created: 06/02/2018
+LastModified: 06/02/2018
+ */
+var DrawProductCard = function (name, price, upc, imageSrc, siteLink, jqueryRef)
 {
     var divCol = $("<div class='col-md-3 product-grid'>");
     var divImage = $("<div class='image'>");
@@ -211,11 +243,70 @@ var DrawProductCard = function (name, price, upc, imageSrc, siteLink)
     aSiteLink.text("BUY");
     divCol.append(aSiteLink);
 
-    $(".hot").append(divCol);
-
-
+    //Category class or ID needed to determine where to put Product card
+    jqueryRef.append(divCol);
 
 }
+
+/*
+Description: This ONCLICK function is to execute a search
+Created By: Aaron Colbert
+Created: 06/02/2018
+LastModified: 06/02/2018
+ */
+$("#submit").on("click", function (event)
+{
+    event.preventDefault();
+
+    var searchTerm = $("#searchInput").val().trim();
+    if (searchTerm)
+    {
+        ProductSearch(searchTerm);
+    }
+});
+
+/*
+Description: This function calls a set of functions that call ajax requests to populate the view with products
+Parameters: searchTerm - string the keyword to be used in the api calls
+Created By: Aaron Colbert
+Created: 06/02/2018
+LastModified: 06/02/2018
+ */
+var ProductSearch = function (searchTerm)
+{
+    if (searchTerm)
+    {
+        jqueryRef = $("");//Somewhere on the SearchResultsPage
+
+        SendWalmartParams(searchTerm, jqueryRef); //Send limit if we want to limit # of search results
+        //PAGE Navigtion to search results 
+        //Call functions of ajax calls for keyword searchs
+        //Pass the jquery of where to populate the products
+    }
+}
+
+/*
+Description: This function calls a set of functions that call ajax requests to Intialize the Productsd within the page
+Created By: Aaron Colbert
+Created: 06/02/2018
+LastModified: 06/02/2018
+ */
+var InitializePage = function ()
+{
+    SendWalmartParams("", 3,"",$(""))//Add CategoryId 
+    //Add Ebay Initial call for Specified Category
+
+    SendWalmartParams("", 3,"",$(""))//Add CategoryId 
+    //Add Ebay Initial call for Specified Category
+
+    SendWalmartParams("", 3,"",$(""))//Add CategoryId 
+    //Add Ebay Initial call for Specified Category
+}
+
+
+
+
+
 
 
 ///////////////////////////////////////////AARON WORKING SECTION////////////////////////////////////////////////////
