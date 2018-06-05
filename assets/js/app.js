a06e87ef5b98e8d89c03a5032344ed5653c694bd
Rabeea Wajeeha <rabeea.uoft@gmail.com>
2018-06-05 17:19:57 -0400

    updated index, product and search

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,3 +69,26 @@ firebase.initializeApp(config);
     $("#item-display").text(snapshot.val().item);
   
   });
+
+  $(document).ready(function(){
+    $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+  });
